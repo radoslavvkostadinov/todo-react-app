316b628615942d140377b0f7676adddd4aa326c0
Radoslav Kostadinov <bounslay@gmail.com>
2024-09-03 21:13:02 +0300

    Add TodoList and TodoListItem components

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 
 1. Initialize vite project
 2. Create Header Component
+3. Create TodoList Component
+4. Create TodoListItem Component
---
+++ src/components/TodoList.jsx
@@ -0,0 +1,8 @@
+export default function TodoList() {
+    return (
+        <div className="todo-list-container">
+            <div className="todo-list-item">
+            </div>
+        </div>
+    )
+}
