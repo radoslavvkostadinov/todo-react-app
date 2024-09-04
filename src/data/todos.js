37573fa787025635354832396723c6f7829e25f5
Radoslav Kostadinov <bounslay@gmail.com>
2024-09-04 18:52:57 +0300

    Add id generator function

--- src/data/todos.js
+++ src/data/todos.js
@@ -1,24 +1,27 @@
+let id = 0;
+const idGenerator = () => (() => id++)();
+
 export const todos = [
     {
-        id: 1,
+        id: idGenerator(),
         text: 'Doctor Appointment',
         completed: false,
         dueDate: '5:23 AM, 01/06/2022'
     },
     {
-        id: 2,
+        id: idGenerator(),
         text: 'Meeting at School',
         completed: false,
         dueDate: '6:00 AM, 02/06/2022'
     },
     {
-        id: 3,
+        id: idGenerator(),
         text: 'Create a react project',
         completed: false,
         dueDate: '1:23 PM, 10/06/2022'
     },
     {
-        id: 4,
+        id: idGenerator(),
         text: 'Create a Todo App',
         completed: false,
         dueDate: '4:23 AM, 11/06/2022'
